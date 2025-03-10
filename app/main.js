e5661bc3fbfac50a29f8e3091e40334e8c6475eb
daviidy <yaodavidarmel@gmail.com>
2025-03-10 15:21:20 -0500

    handle double quotes

--- app/main.js
+++ app/main.js
@@ -28,7 +28,7 @@ class Shell {
 
   getCommandNameAndArgs(input) {
     const args = input.match(/(?:[^\s'"]+|'[^']*'|"[^"]*")+/g) || [];
-    const commandName = args.shift().replace(/['"]/g, '');
+    const commandName = args.shift();
     return { commandName, args };
   }
   
