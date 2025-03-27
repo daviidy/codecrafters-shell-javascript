450328a2dc90aedcfceba37015200aa4531edbb9
daviidy <yaodavidarmel@gmail.com>
2025-03-27 14:27:51 -0500

    builtin completion

--- app/OutputHandler.js
+++ app/OutputHandler.js
@@ -86,7 +86,7 @@ class OutputHandler {
                 this._initializeFile();
             }
             if (newLine) {
-                process.stderr.write(message + " " + '\n');
+                process.stderr.write(message + "$ " + '\n');
             } else {
                 process.stderr.write(message);
             }
