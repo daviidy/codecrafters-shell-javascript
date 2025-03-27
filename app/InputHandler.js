6f7cb56312dcf7c775e58fe695299ca618bf9f55
daviidy <yaodavidarmel@gmail.com>
2025-03-26 22:36:11 -0500

    builtin completion

--- app/InputHandler.js
+++ app/InputHandler.js
@@ -19,15 +19,12 @@ class InputHandler {
                     if (suggestions.length === 1) {
                         // Autocomplete if there's only one suggestion
                         this.inputBuffer = suggestions[0] + ' ';
-                        this.readline.write(null, { ctrl: true, name: 'u' }); // Clear the current line
-                        this.readline.write(this.inputBuffer); // Write the autocompleted input
+                        process.stdout.write(`\r${prompt} ${this.inputBuffer}`); // Reprint the prompt and input
                     } else if (suggestions.length > 1) {
                         console.log('\n' + suggestions.join(' '));
-                        this.readline.prompt();
-                        this.readline.write(this.inputBuffer); // Reprint the current input
+                        process.stdout.write(`${prompt} ${this.inputBuffer}`); // Reprint the prompt and input
                     }
                 } else if (key.name === 'backspace') {
-                    console.log('dave return');
                     // Handle Backspace
                     this.inputBuffer = this.inputBuffer.slice(0, -1);
                     this.readline.write(null, { ctrl: true, name: 'u' }); // Clear the current line
